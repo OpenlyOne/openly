c82710680fb566791c68defb7db131751c795031
Finn Woelm <public@finnwoelm.com>
2020-08-25 12:11:04 +0200

    Add babel-plugin-module-resolver
    Improve module resolution for helpers and components.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 import { Typography } from "@material-ui/core";
+import Layout from "components/Layout";
 
-const Index = () => <Typography variant="body1">Hello World!</Typography>;
+const Index = () => (
+  <Layout>
+    <Typography variant="body1">Hello World!</Typography>
+  </Layout>
+);
 
 export default Index;
