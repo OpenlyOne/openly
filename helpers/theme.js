16447c551acf9799c33f2cdc0c0788ce04eff0f4
Finn Woelm <public@finnwoelm.com>
2020-08-25 12:11:04 +0200

    Set up MaterialUI and styled components
    Configure NextJS for use with materialUI and styled components.

---
+++ helpers/theme.js
@@ -0,0 +1,63 @@
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core'
+
+const theme = createMuiTheme({
+  typography: {
+    h1: {
+      fontSize: '3.7rem',
+      fontWeight: 500
+    },
+    h2: {
+      fontSize: '1.9rem',
+      fontWeight: 400
+    },
+    h3: {
+      fontSize: '1.25rem',
+      fontWeight: 400
+    },
+    h4: {
+      fontSize: '1.15rem',
+      fontWeight: 400
+    },
+    body1: {
+      lineHeight: 1.25,
+    },
+    subtitle1: {
+      fontSize: '1.25rem',
+      fontWeight: 300,
+      lineHeight: 1.33
+    },
+    subtitle2: {
+      fontSize: '1rem'
+    },
+    caption: {
+      fontSize: '.7rem',
+      letterSpacing: 0,
+      lineHeight: 1.2,
+    },
+    overline: {
+      fontSize: '.7rem',
+      letterSpacing: 0,
+      textTransform: 'uppercase',
+      lineHeight: 1,
+      marginBottom: 8,
+    }
+  },
+  props: {
+    MuiTypography: {
+      variantMapping: {
+        h1: 'h1',
+        h2: 'h2',
+        h3: 'h3',
+        h4: 'h4',
+        subtitle1: 'p',
+        subtitle2: 'p',
+        caption: 'p',
+        overline: 'p',
+      },
+    },
+  },
+});
+
+const themeWithResponsiveFontSizes = responsiveFontSizes(theme)
+
+export default themeWithResponsiveFontSizes
---
+++ pages/_app.js
@@ -0,0 +1,40 @@
+import React from "react";
+import App from "next/app";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../helpers/theme";
+
+class MyApp extends App {
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <>
+        <Head>
+          <title>My page</title>
+          {/* Use minimum-scale=1 to enable GPU rasterization */}
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+          />
+        </Head>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </>
+    );
+  }
+}
+
+export default MyApp;
